3d3fcff925cccc66c499db1132137b7b196d59c0
Jeremy Parada <jeremy.parada@stgchile.cl>
2025-11-26 03:49:39 -0300

    Feat: Agregar componente DireccionesManager con CRUD completo

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,6 +3,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import { useNotification } from '../hooks/useNotification';
 import { AuthContext } from '../context/AuthContext';
 import fetchWithAuth from '../utils/api';
+import DireccionesManager from '../components/DireccionesManager';
 
 const Profile = () => {
   const { exito } = useNotification();
